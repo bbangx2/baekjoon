447e449d48e1d1aaa9cdcf72226a7b5247321324
bbangx2 <59238662+bbangx2@users.noreply.github.com>
2025-07-24 15:02:09 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int n;
+	cin >> n;
+
+	int a, b;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+		cout << "You get " << a / b << " piece(s) and your dad gets "
+			<< a % b << " piece(s)." << endl;
+	}
+	return 0;
+}
