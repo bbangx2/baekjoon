f7a3abecd4a311efc4129dc42990bdcd5a2fcbfc
bbangx2 <59238662+bbangx2@users.noreply.github.com>
2025-07-24 14:55:55 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int sum = 0;
+	int a;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> a;
+		sum += a;
+	}
+
+	cout << sum;
+
+	return 0;
+}
